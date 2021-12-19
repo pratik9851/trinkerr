a6d38a90b4985f07e66445ef232f408b2c528445
Pratik <sahap187@gmail.com>
2021-12-20 04:05:28 +0530

    last commit

--- src/components/searchStock.jsx
+++ src/components/searchStock.jsx
@@ -6,6 +6,7 @@ export default function SearchStock(){
     
     const [input, setinput] = useState("");
     const [data1, setdata1] = useState([])
+    const [wishlist,setwishlist]=useState([])
   
     const handleData = (e) => {
       // console.log(e.target.value);
@@ -28,12 +29,22 @@ export default function SearchStock(){
         }, d);
       };
     }
+    function handelWishlist(el){
+        if(data.forEach(item=>item[0]===el[0])){
+            return
+        }else{
+        setwishlist((prev)=>{
+            return[...prev,el]
+        })
+        setinput("")
+        }
+    }
 
     return(
         <div className={style.container1}>
             <div className={style.container2}>
                 <div className={style.inpdiv}>
-                  <input type="text" className={style.inp} onChange={debounce(handleData,300)} placeholder="seach stocks" />
+                  <input type="text" className={style.inp} onChange={debounce(handleData,300)} placeholder="serach stocks" />
                   <div className={`${style.dropdown} ${input !== "" && style.showdropdown}`} >
             {data1.map((el, i) => (
               <div key={i} className={style.stock}>
@@ -44,12 +55,29 @@ export default function SearchStock(){
                 <div className={style.stock_1}>
                   <div>{el[1]}</div>
                   <div>{((el[1] - el[2]) / el[2]).toFixed(3)} %</div>
+                  
                 </div>
-                
+                <button className={style.addbutton} onClick={()=>handelWishlist(el)}>Add</button>
               </div>
             ))}
           </div>
                 </div>
+                <div className={style.wishlist} >
+            {wishlist.map((el, i) => (
+              <div key={i} className={style.stock}>
+                <div className={style.stock_1}>
+                  <div>{el[0].split("::")[0]}</div>
+                  <div>{el[0].split("::")[1]}</div>
+                </div>
+                <div className={style.stock_1}>
+                  <div>{el[1]}</div>
+                  <div>{((el[1] - el[2]) / el[2]).toFixed(3)} %</div>
+                  
+                </div>
+                <button className={style.deletebutton} >Delete</button>
+              </div>
+            ))}
+          </div>
 
             </div>
         </div>
